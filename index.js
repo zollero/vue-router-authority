b9f1e2bc6d2f722f3abb33302469e118d514638b
zollero <corona7@163.com>
2017-08-27 21:49:48 +0800

    add entry files

---
+++ index.js
@@ -0,0 +1,9 @@
+/*!
+ * vue router guard
+ * Copyright(c) 2017 zollero
+ * MIT Licensed
+ */
+
+'use strict';
+
+module.exports = require('./src');
--- src/index.js
+++ src/index.js
@@ -2,4 +2,63 @@
 
 export default class VueRouterGuard {
 
+  constructor(options = {}) {
+
+  }
+
+  RouterAuthority (to, from, next) {
+    let { pageOperations } = store.state.login
+
+    if (to.path === '/' || to.path === '/welcome') {
+      toggleLoading()
+      next()
+      return
+    }
+
+    if (pageOperations.length === 0) {
+      if (from.name !== null) {
+        toggleLoading()
+        next('/welcome')
+        return
+      } else {
+        // 刷新页面，这时还没获得权限内容，需要在这里手动调权限接口来获得
+        loginApi.getOperation().then(response => {
+          const { code, data } = response
+          if (code === '00000') {
+            pageOperations = data.pageOperations
+            toggleLoading()
+            routerHandler(to, from, next, pageOperations)
+          } else {
+            pageOperations = []
+            toggleLoading()
+            next()
+          }
+        })
+      }
+    } else {
+      routerHandler(to, from, next, pageOperations)
+    }
+  })
+}
+
+function routerHandler(to, from, next, pageOperations) {
+  let hasAccess = false
+  pageOperations.forEach(v => {
+    if (v === to.name) {
+      hasAccess = true
+    }
+  })
+
+  if (hasAccess) {
+    next()
+  } else {
+    Message.info({
+      message: '没有权限'
+    })
+    if (from.name) {
+      next(from.path)
+    } else {
+      next('/welcome')
+    }
+  }
 }
